5fa9fdc95f6714b7a8e6da14838802277eda4822
Vikas Gavandi <85719124+login-web@users.noreply.github.com>
2021-06-11 08:19:02 +0530

    Revert "Added new work"
    This reverts commit 78f7f1ddccd8398e3168a473570a2c0422b8c8ab.

--- README.md
+++
@@ -1,2 +0,0 @@
-# quiz-app
-this application is a quiz game which is created using html css and javascript
--- highscore.js
+++
@@ -1,8 +0,0 @@
-const highscorelist = document.getElementById("highscorelist");
-const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
-
-highscorelist.innerHTML = highscores
-  .map(score => {
-    return `<li class="high-score">${score.name} - ${score.score}</li>`;
-  })
-  .join("");
